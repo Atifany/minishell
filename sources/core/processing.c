007af32c7b78e408e6cf933670784214aaecb6e6
Atifany <bordackov.misha2014@yandex.ru>
2022-06-21 16:33:57 +0300

    added ft_isspace() func

--- sources/core/processing.c
+++ sources/core/processing.c
@@ -53,8 +53,6 @@ static char	iterate_exec_line(char **exec_line, t_line *line)
 		if (line->is_redirecting){
 			redirect_output(line, CLOSE);
 		}
-		write(1, "error\n", 6);
-		// PRINT ERROR HERE
 		if (ret) // switch returned exit code.
 			return (1);
 	}
